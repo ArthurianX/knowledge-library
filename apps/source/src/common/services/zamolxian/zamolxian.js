5cefc71f0782a9d12b72a9d92ae82a4ec6a8b36a
Arthur Henning <h_arthur_12@yahoo.com>
2014-03-12 12:35:32 +0200

    Added empty factory services. #23

--- apps/source/src/common/services/zamolxian/zamolxian.js
+++ apps/source/src/common/services/zamolxian/zamolxian.js
@@ -14,4 +14,84 @@ angular.module('zamolxian.services', [])
             });
         }
     };
+})
+
+.factory('tutorials', function ($http, $q) {
+    return {
+        getAll: function () {
+            // client id
+            // client secret
+
+            return $q.all([
+                    $http.get(/* url */)
+                ]).then(function (results) {
+                // process results
+                return results;
+            });
+        }
+    };
+})
+
+.factory('community', function ($http, $q) {
+    return {
+        getAll: function () {
+            // client id
+            // client secret
+
+            return $q.all([
+                    $http.get(/* url */)
+                ]).then(function (results) {
+                // process results
+                return results;
+            });
+        }
+    };
+})
+
+.factory('tipstricks', function ($http, $q) {
+    return {
+        getAll: function () {
+            // client id
+            // client secret
+
+            return $q.all([
+                    $http.get(/* url */)
+                ]).then(function (results) {
+                // process results
+                return results;
+            });
+        }
+    };
+})
+
+.factory('modulesupdates', function ($http, $q) {
+    return {
+        getAll: function () {
+            // client id
+            // client secret
+
+            return $q.all([
+                    $http.get(/* url */)
+                ]).then(function (results) {
+                // process results
+                return results;
+            });
+        }
+    };
+})
+
+.factory('readinglist', function ($http, $q) {
+    return {
+        getAll: function () {
+            // client id
+            // client secret
+
+            return $q.all([
+                    $http.get(/* url */)
+                ]).then(function (results) {
+                // process results
+                return results;
+            });
+        }
+    };
 });
