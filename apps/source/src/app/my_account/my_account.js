3f3d05bbbc911c707f31eb6a0536e4d1d888cb3c
GBora <gpbora@gmail.com>
2014-03-21 14:52:47 +0200

    Finished front end work on form. Now comes the validation

--- apps/source/src/app/my_account/my_account.js
+++ apps/source/src/app/my_account/my_account.js
@@ -19,17 +19,42 @@ angular.module('zamolxian.my_account', [
     })
 
     .controller("AccountCtrl", function AccountCtrl($scope, promiseTracker, $sanitize, countryListing) {
-            $scope.userName = "George Bora";
-            $scope.userMail = "gbora@pititechnologies.ro";
+        $scope.userName = "George Bora";
+        $scope.userMail = "gbora@pititechnologies.ro";
 
-            $scope.countryList = countryListing.countryList;
-            $scope.userCountry = $scope.countryList[89];
 
-            $scope.contactZalmoxis = function() {
-                   $scope.userName = $sanitize($scope.newUserName);
-                   $scope.userMail = $sanitize($scope.newUserMail);
-                  console.log($sanitize($scope.newUserName));
-                  console.log($sanitize($scope.newUserMail));
-                  $scope.contact = false;
-            };
+        $scope.motivesList = [
+            { "name": "Hire Me Pls", "options": [
+                {"name": "Web Developer"},
+                {"name": "Front End Developer"},
+                {"name": "Back End Developer"},
+                {"name": "Designer"},
+                {"name": "DB Admin"}
+            ]},
+
+            {"name": "Vengeance", "options": [
+                {name: "You killed my mother"},
+                {name: "You killed my father"},
+                {name: "You killed my family"}
+            ]},
+
+            {"name": "Miscelonous", "options": [
+                {"name": "Miscelonious"},
+                {"name": "Random"},
+                {"name": "Anarchy"}
+            ]}
+
+        ];
+
+        $scope.selectedMotive = $scope.motivesList[0];
+
+        $scope.countryList = countryListing.countryList;
+        $scope.userCountry = $scope.countryList[89];
+
+        // I intialize all the error to false
+        $scope.errorList = {"firstName": false, "lastName": false, "mail": false, "phoneNumber": false, "adress": false, "county": false, "country": false};
+
+        $scope.contactZalmoxis = function () {
+            $scope.contact = false;
+        };
     });
