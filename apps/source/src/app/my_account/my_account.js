74fc0be7165086955b62ab77494d0f526f4df19e
GBora <gpbora@gmail.com>
2014-03-24 16:54:52 +0200

    Added geocoding and phone validation

--- apps/source/src/app/my_account/my_account.js
+++ apps/source/src/app/my_account/my_account.js
@@ -5,7 +5,7 @@ angular.module('zamolxian.my_account', [
         'ngSanitize'
     ])
 
-    .config(function config($stateProvider) {
+    .config(function config($stateProvider, $httpProvider) {
         $stateProvider.state('my_account', {
             url: '/my_account',
             views: {
@@ -18,9 +18,10 @@ angular.module('zamolxian.my_account', [
         });
     })
 
-    .controller("AccountCtrl", function AccountCtrl($scope, promiseTracker, $sanitize, countryListing) {
+    .controller("AccountCtrl", function AccountCtrl($scope, $http, promiseTracker, $sanitize, countryListing) {
         $scope.userName = "George Bora";
         $scope.userMail = "gbora@pititechnologies.ro";
+        $scope.contactData = {};
 
 
         $scope.motivesList = [
@@ -49,12 +50,84 @@ angular.module('zamolxian.my_account', [
         $scope.selectedMotive = $scope.motivesList[0];
 
         $scope.countryList = countryListing.countryList;
-        $scope.userCountry = $scope.countryList[89];
+        $scope.cdCountry = $scope.countryList[89];
 
-        // I intialize all the error to false
+        // I initialize all the error to false
         $scope.errorList = {"firstName": false, "lastName": false, "mail": false, "phoneNumber": false, "adress": false, "county": false, "country": false};
 
         $scope.contactZalmoxis = function () {
             $scope.contact = false;
         };
+
+        $scope.checkContactData = function (suspect) {
+            switch (suspect) {
+                case "firstName" :
+                    $scope.errorList["firstName"] = !($scope.cdFirstName.length > 5 && $scope.cdFirstName.length < 30);
+                    break;
+
+                case "lastName" :
+                    $scope.errorList["lastName"] = !($scope.cdLastName.length > 5 && $scope.cdLastName.length < 30);
+                    break;
+
+                case "mail" :
+                    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    $scope.errorList["mail"] = !(re.test($scope.cdMail));
+                    break;
+
+                case "adress" :
+                    // Do something here with google API
+                    console.log("Checking the adress");
+
+                    var adress = $scope.cdAdress + ',' + $scope.cdCity + ',' + $scope.cdCounty + ',' + $scope.cdCountry;
+
+                    geocoder = new google.maps.Geocoder();
+
+                    geocoder.geocode({ 'address': adress}, function (results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            console.log("address is ok");
+                        } else {
+                            $scope.errorList["adress"] = true;
+                            $scope.errorList["county"] = true;
+                            $scope.errorList["country"] = true;
+                            console.log("adress is not ok");
+
+                        }
+                    });
+                    break;
+
+                case "phone" :
+
+                    Locale = 'en-US';
+
+                    $http.get('http://www.phone-validator.net/api/verify', { PhoneNumber: $scope.cdPhone, CountryCode: $scope.cdCountry.cca2,
+                        Locale: Locale, APIKey: 'pv-1571f24e7e74c413343dce8f4214f31a'}).success(function (json) {
+                        console.log(json);
+                        if (typeof(json.status) != "undefined") {
+                            if (json.status === 'FAIL') {
+                                console.log("number is invalid");
+                                $scope.errorList["phoneNumber"] = true;
+                            } else {
+                                console.log("number is valid");
+                            }
+                        }
+                    }).error(function (data, status, headers, config) {
+                        $scope.status = status;
+                    });
+
+
+                    break;
+
+                default :
+                    console.log("default case has been triggered");
+                    break;
+            }
+        };
     });
+
+
+var geocoder;
+var initialize = function () {
+    console.log("loading geocoder...");
+    geocoder = new google.maps.Geocoder();
+    console.log("geocoder loaded");
+};
