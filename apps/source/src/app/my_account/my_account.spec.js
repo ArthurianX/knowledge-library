71e37348d29a5f684e0eec573ad013c014492425
Arthur Henning <h_arthur_12@yahoo.com>
2014-03-26 10:54:55 +0200

    Modified AccountCtrl Test to initialize controller in beforeEach function.

--- apps/source/src/app/my_account/my_account.spec.js
+++ apps/source/src/app/my_account/my_account.spec.js
@@ -1,5 +1,18 @@
-describe('AccountCtrl Test', function() {
-   beforeEach(module('zamolxian.my_account'));
+describe('AccountCtrl Test', function () {
+
+    var scope;
+    var ctrl;
+
+    beforeEach(function () {
+        module('zamolxian.my_account');
+        inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new;
+            ctrl = $controller('AccountCtrl', {
+                $scope: scope, countryListing: mockCountryListing,
+                PhoneService: mockPhoneService
+            });
+        });
+    });
 
     var mockCountryListing = {
         countryList: [
@@ -9,7 +22,7 @@ describe('AccountCtrl Test', function() {
     };
 
     var mockPhoneService = {
-        isValidPhoneNr: function(phoneNr, countryCode) {
+        isValidPhoneNr: function (phoneNr, countryCode) {
             if (phoneNr.length == 10 && countryCode.length == 2) {
                 return true;
             }
@@ -17,43 +30,52 @@ describe('AccountCtrl Test', function() {
         }
     };
 
-    it('should return an array of two countries',
-        inject(function($rootScope, $controller) {
-            var scope = $rootScope.$new;
-            var ctrl = $controller('AccountCtrl', {
-                $scope: scope, countryListing: mockCountryListing,
-                PhoneService: mockPhoneService
-            });
+    it('should return an array of two countries', function () {
+        expect(scope.countryList.length).toBe(2);
+    });
 
-            expect(scope.countryList.length).toBe(2);
-        }));
+    it('should return an array of four motives', function () {
+        expect(scope.motivesList.length).toBe(4);
+    });
 
-    it('should return an array of four motives',
-        inject(function($rootScope, $controller) {
-            var scope = $rootScope.$new;
-            var ctrl = $controller('AccountCtrl', {
-                $scope: scope, countryListing: mockCountryListing,
-                PhoneService: mockPhoneService
-            });
-
-            expect(scope.motivesList.length).toBe(4);
-        }));
-
-    it('should initialize errorList with false or undefinde values',
-        inject(function($rootScope, $controller) {
-            var scope = $rootScope.$new;
-            var ctrl = $controller('AccountCtrl', {
-                $scope: scope, countryListing: mockCountryListing,
-                PhoneService: mockPhoneService
-            });
-
-            var error = false;
-            for (var property in scope.errorList) {
-                if (scope.errorList.hasOwnProperty(property)) {
-                    error = error || scope.errorList[property];
-                }
+    it('should initialize errorList with false or undefinde values', function () {
+        var error = false;
+        for (var property in scope.errorList) {
+            if (scope.errorList.hasOwnProperty(property)) {
+                error = error || scope.errorList[property];
             }
+        }
 
-            expect(error).not.toBeTruthy();
-        }));
+        expect(error).not.toBeTruthy();
+    });
+
+//    it('should should return true for valid phone number',
+//        inject(function ($rootScope, $controller) {
+//
+//            //Assign false by default to the expected value.
+//
+//            var valueOutter = false, deferred = q.defer();
+//
+//            var scope = $rootScope.$new;
+//            var ctrl = $controller('AccountCtrl', {
+//                $scope: scope, countryListing: mockCountryListing,
+//                PhoneService: mockPhoneService
+//            });
+//
+//            var phoneNr = "0744555666";
+//            var countryCode = "RO";
+//            //var isValid = scope.isValidPhoneNr(phoneNr, countryCode);
+//            //expect(isValid).toBeTruthy();
+//
+//
+//            deferred.promise.then(function(value){
+//                valueOutter = value;
+//            }, function(){console.log('error!');});
+//
+//            expect(valueOutter).toBeFalsy();
+//            deferred.resolve(false);
+//            expect(valueOutter).toBeTruthy();
+//
+//        }));
+
 });
